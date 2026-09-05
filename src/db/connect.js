8b81db3adef4c46d2e3ae30b51a048d899eee5d7
KevinBezerra <kevinmbezerra@gmail.com>
2026-09-05 16:52:28 -0300

    Connected app to MongoDB

---
+++ src/db/connect.js
@@ -0,0 +1,26 @@
+import { MongoClient } from 'mongodb';
+import { setServers } from "node:dns/promises";
+
+setServers(["1.1.1.1", "8.8.8.8"]);
+
+let database;
+
+const connectToDb = async () => {
+  const connectionString = process.env.MONGODB_URI;
+  if (!connectionString) {
+    throw new Error('MONGODB_URI is required.');
+  }
+  const client = new MongoClient(connectionString);
+  await client.connect();
+  database = client.db(process.env.MONGODB_DB_NAME || 'practice');
+  return database;
+};
+
+const getDb = () => {
+  if (!database) {
+    throw new Error('Database not initialized. Call connectToDb first.');
+  }
+  return database;
+};
+
+export { connectToDb, getDb };
