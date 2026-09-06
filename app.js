1380c7c7767bd8165455e1bcd215931d9d519cff
KevinBezerra <kevinmbezerra@gmail.com>
2026-09-06 19:43:18 -0300

    Added GET /books route

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from 'express';
+import router from './src/router.js';
 
 const app = express();
 
-// Middleware to parse incoming JSON requests
 app.use(express.json());
+app.use(router);
 
-// Basic root route
 app.get('/', (req, res) => {
   return res.status(200).json({ message: 'Server is running' });
 });
